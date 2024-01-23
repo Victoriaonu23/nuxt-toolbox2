d2c121e6baca7287eaf300502dfefa23d586ff1f
Victoriaonu23 <136612702+Victoriaonu23@users.noreply.github.com>
2024-01-23 21:06:18 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,15 @@
       <h1>Nuxt Toolbox</h1>
       <p>
         Hi 👋! This page gives you a
-        <a href="https://naijaspider.com/property/">Nuxt</a> platform with the scaffolding for
-        <a href="https://opiopio.ng/"
-          >Functions</a>,
+        <a href="https://naijaspider.com/property/">Naij</a> platform with the scaffolding for
+        <a href="https://opiopio.ng/">News</a>,
         Our aim was to give you the code you would need to hit the ground running
         with a few fun features.
       </p>
 
       <p>
-        You can find the code for this project on GitHub at
-        <a href="https://github.com/netlify-templates/nuxt-toolbox"
-          >https://github.com/netlify-templates/nuxt-toolbox</a
-        >! Happy coding!
+       
+        ! Happy coding!
       </p>
     </div>
     <FeedbackForm />
